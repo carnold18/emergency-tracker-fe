57298932b570fe6a0a5ef20d70573267e310fc5b
Christopher Arnold <chrisarnold2010@gmail.com>
2018-12-04 14:25:57 -0600

    added .env file

--- src/AdminContainer.js
+++ src/AdminContainer.js
@@ -240,12 +240,12 @@ class AdminContainer extends Component {
         // console.log(this.props.currentUser.id)
         // console.log(this.props.currentUser.user_type)
         // console.log(this.props.currentUser.zone_id)
-        console.log(GEO_API_KEY)
+        // console.log(GEO_API_KEY)
         // console.log(dotenv)
         console.log(process)
         console.log(process.env)
         console.log(process.env.production)
-        const API_KEY = GEO_API_KEY
+        const API_KEY = process.env.GEO_API_KEY
         console.log(API_KEY)
         const URL = 'https://maps.googleapis.com/maps/api/js?key='+API_KEY+'&v=3.exp&libraries=geometry,drawing,places'
 
