47386ccbfec04ea0e745506de11d83891fb4b899
Christopher Arnold <chrisarnold2010@gmail.com>
2018-12-04 03:04:26 -0600

    added .env file

--- src/AdminContainer.js
+++ src/AdminContainer.js
@@ -7,7 +7,7 @@ import MessagePost from './MessagePost';
 import StatsChart from './StatsChart';
 import { GEO_API_KEY } from "./ApiKey";
 // import config from './config.js';
-const aws = require('aws-sdk');
+// const aws = require('aws-sdk');
 
 class AdminContainer extends Component {
 
@@ -234,14 +234,14 @@ class AdminContainer extends Component {
         // console.log(this.props.currentUser.id)
         // console.log(this.props.currentUser.user_type)
         // console.log(this.props.currentUser.zone_id)
-        let s3 = new aws.S3({
-            accessKey: process.env.GEO_API_KEY
-        }) 
+        // let s3 = new aws.S3({
+        //     accessKey: process.env.GEO_API_KEY
+        // }) 
         console.log(GEO_API_KEY)
         console.log(process.env)
-        console.log(s3)
-        console.log(s3.accessKey)
-        const API_KEY = s3.accessKey
+        // console.log(s3)
+        // console.log(s3.accessKey)
+        const API_KEY = 'hello'
         console.log(API_KEY)
         const URL = 'https://maps.googleapis.com/maps/api/js?key='+API_KEY+'&v=3.exp&libraries=geometry,drawing,places'
 
